bc1eb689d1c0501a7abc6363954a9171adbab327
katie leong <katieleong@yahoo.com>
2024-08-05 13:22:09 -0700

    add condition in get all reviews of current user route and include/exclude info in results

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -5,16 +5,32 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
-const { Review } = require('../../db/models');
+const { Review, User, Spot } = require('../../db/models');
 
 const router = express.Router();
 
 //get all reviews of current user
 router.get('/current', requireAuth, async (req,res) => {
   const {user} = req;
-  const currentUserId = user.id;
-  const reviews = await Review.findAll({where: {userId: currentUserId}})
-  return res.json({'Reviews': reviews});
+
+  if (user) {
+    const currentUserId = user.id;
+    const reviews = await Review.findAll({
+      where: {
+        userId: currentUserId
+      },
+      include: [{
+        model: User,
+        attributes: ['id', 'firstName', 'lastName']
+      }, {
+        model: Spot,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'description']
+        }
+      }]})
+    // add ReviewImage in include later
+    return res.json({'Reviews': reviews});
+  }
 })
 
 module.exports = router;
