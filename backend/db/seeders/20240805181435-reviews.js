5fe5ca3653199b15a41c553ac42178df4f30451e
katie leong <katieleong@yahoo.com>
2024-08-05 12:15:21 -0700

    add seeds for reviews

--- backend/db/seeders/20240805181435-reviews.js
+++ backend/db/seeders/20240805181435-reviews.js
@@ -1,6 +1,46 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+
+const {Review} = require('../models');
+const reviews = [
+  {
+    "id": 1,
+    "userId": 1,
+    "spotId": 1,
+    "review": "This was an awesome spot!",
+    "stars": 5
+  },
+  {
+    "id": 2,
+    "userId": 2,
+    "spotId": 2,
+    "review": "Great experience, highly recommend.",
+    "stars": 4
+  },
+  {
+    "id": 3,
+    "userId": 3,
+    "spotId": 3,
+    "review": "It was decent, but could be better.",
+    "stars": 3
+  },
+  {
+    "id": 4,
+    "userId": 4,
+    "spotId": 4,
+    "review": "Not what I expected, quite disappointing.",
+    "stars": 2
+  },
+  {
+    "id": 5,
+    "userId": 5,
+    "spotId": 5,
+    "review": "Terrible experience, will not return.",
+    "stars": 1
+  }
+]
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
@@ -12,6 +52,7 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    await Review.bulkCreate(reviews, {validate: true});
   },
 
   async down (queryInterface, Sequelize) {
@@ -21,5 +62,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await Review.destroy({where: {}, truncate: true});
   }
 };
