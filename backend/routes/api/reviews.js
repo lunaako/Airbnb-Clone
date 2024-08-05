b503b734e0c5b4139d4b9ef84e8c24971e0a167e
katie leong <katieleong@yahoo.com>
2024-08-05 12:39:07 -0700

    create reviews router

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { Review } = require('../../db/models');
+
+const router = express.Router();
+
+
+
+
+module.exports = router;
