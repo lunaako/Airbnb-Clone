2f4ad201a5bd9480b2a7fb802760e7739c2fc765
Yicheng Wang <yichengw1011@gmail.com>
2024-08-05 13:14:56 -0700

    finished the validation and invalid credentialerror for log in router in session router

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -13,10 +13,10 @@ const validateLogin = [
   check('credential')
     .exists({ checkFalsy: true })
     .notEmpty()
-    .withMessage('Please provide a valid email or username.'),
+    .withMessage('Email or username is required.'),
   check('password')
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a password.'),
+    .withMessage('Password is required.'),
   handleValidationErrors
 ];
 
@@ -37,7 +37,7 @@ router.post(
     });
 
     if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-      const err = new Error('Login failed');
+      const err = new Error('Invalid credentials');
       err.status = 401;
       err.title = 'Login failed';
       err.errors = { credential: 'The provided credentials were invalid.' };
