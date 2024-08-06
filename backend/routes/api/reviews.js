d924f99abf4541cc971f5bffd3dc8dbec02f65ca
katie leong <katieleong@yahoo.com>
2024-08-05 17:40:15 -0700

    fixed validations for validateReview

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -11,13 +11,9 @@ const router = express.Router();
 
 const validateReview = [
   check('review')
-    // .exists({ checkFalsy: true })
-    // .notEmpty()
-    .isString()
+    .if(check('review').exists()).isLength({min: 1})
     .withMessage('Review text is required'),
   check('stars')
-    // .exists({ checkFalsy: true })
-    // .notEmpty()
     .isInt({min: 1, max: 5})
     .withMessage('Stars must be an integer from 1 to 5'),
 handleValidationErrors
