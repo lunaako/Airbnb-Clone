9af85c7eb567f825185d55d701f7eb806bc2e46a
Yicheng Wang <yichengw1011@gmail.com>
2024-08-23 14:31:48 -0700

    setup restore the session user

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -31,6 +31,14 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+//!thunk action creator ----> restore user session (GET api/session router)
+export const restoreUser = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session");
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
